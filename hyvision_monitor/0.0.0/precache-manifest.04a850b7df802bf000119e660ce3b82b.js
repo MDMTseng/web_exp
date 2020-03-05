1d113086d5be7b3476070b4fbb901c192ebaf2e7
Chia-Pin Tseng <chiapintseng@gmail.com>
2020-03-05 11:26:28 +0800

    no message

---
+++ hyvision_monitor/0.0.0/precache-manifest.04a850b7df802bf000119e660ce3b82b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482aafb9d931e1c4bb134b39273c4d8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44d4c008c42cf6a7b236",
+    "url": "./static/css/2.ec869857.chunk.css"
+  },
+  {
+    "revision": "57b1e1abbebc309704f5",
+    "url": "./static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "44d4c008c42cf6a7b236",
+    "url": "./static/js/2.7bf4a06a.chunk.js"
+  },
+  {
+    "revision": "5a9d6b195ad2356646351e76d60a52e6",
+    "url": "./static/js/2.7bf4a06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b1e1abbebc309704f5",
+    "url": "./static/js/main.1659c79d.chunk.js"
+  },
+  {
+    "revision": "f9925d0c46f212329e2b",
+    "url": "./static/js/runtime-main.b810a192.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- hyvision_monitor/0.0.0/service-worker.js
+++ hyvision_monitor/0.0.0/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.668c526693c41c91988597fcf2f2d843.js"
+  "./precache-manifest.04a850b7df802bf000119e660ce3b82b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
