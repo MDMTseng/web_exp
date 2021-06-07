f45046419d50059300deceec86243e3a429c2b5e
chiapintseng <chiapintseng@gmail.com>
2021-06-08 03:17:56 +0800

    InspMon:update

---
+++ hyvision_monitor/0.0.1/precache-manifest.b9c1d326b646965894c0a14cff7eebaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676fec76e6f1e4971012efc0eae9c74a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74e74d03294324b4d2a0",
+    "url": "./static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "1e40acc4f08cf45665be",
+    "url": "./static/css/main.557310a9.chunk.css"
+  },
+  {
+    "revision": "74e74d03294324b4d2a0",
+    "url": "./static/js/2.e29cc1a1.chunk.js"
+  },
+  {
+    "revision": "f5e5aec481b215deaa59089013b46063",
+    "url": "./static/js/2.e29cc1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e40acc4f08cf45665be",
+    "url": "./static/js/main.6e792002.chunk.js"
+  },
+  {
+    "revision": "0a82f1893ae05ef6684d",
+    "url": "./static/js/runtime-main.6b7762c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
